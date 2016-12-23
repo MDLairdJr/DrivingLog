07b7a919c0ec573f63fc59c8e78f3dbb553aa209
Mike Laird <mike.laird@gmail.com>
2016-12-23 18:57:23 -0500

    Add CursorLoader and remove temporary display database method

--- app/src/main/java/com/mikelaird/drivinglog/TripDetailsActivity.java
+++ app/src/main/java/com/mikelaird/drivinglog/TripDetailsActivity.java
@@ -1,5 +1,8 @@
 package com.mikelaird.drivinglog;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +10,14 @@ import android.widget.ListView;
 
 import com.mikelaird.drivinglog.data.TripContract.TripEntry;
 
-public class TripDetailsActivity extends AppCompatActivity {
+public class TripDetailsActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /** Tag for the log messages */
     public static final String LOG_TAG = TripDetailsActivity.class.getSimpleName();
+
+    private static final int TRIP_LOADER = 0;
+    TripCursorAdapter mTripCursorAdapter;
 
     /*
      * @todo When this activity starts, it causes a new trip to start
@@ -21,6 +28,14 @@ public class TripDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
+
+        // kick off the cursor loader
+        getLoaderManager().initLoader(TRIP_LOADER, null, this);
+
+        ListView tripView = (ListView)findViewById(R.id.trip_details_list_view);
+        mTripCursorAdapter = new TripCursorAdapter(this, null);
+        tripView.setAdapter(mTripCursorAdapter);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -38,15 +53,12 @@ public class TripDetailsActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        displayDatabaseInfo();
     }
 
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the trips database.
-     */
-    private void displayDatabaseInfo() {
 
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         // Define a projection that specifies which columns from the database
         // we will actually use for this query.
         String[] projection = {
@@ -55,11 +67,23 @@ public class TripDetailsActivity extends AppCompatActivity {
                 TripEntry.COLUMN_NAME_DURATION,
                 TripEntry.COLUMN_NAME_NOTES };
 
-        Cursor cursor = getContentResolver().query(TripEntry.CONTENT_URI, projection, null,
-                null, null);
+        // this loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(
+                this,                       // parent activity context
+                TripEntry.CONTENT_URI,      // provider contentURI
+                projection,                 // columns to include in the query
+                null,                       // where clause
+                null,                       // where clause arguments
+                null);                      // sort order
+    }
 
-        ListView tripView = (ListView)findViewById(R.id.trip_details_list_view);
-        TripCursorAdapter tripCursorAdapter = new TripCursorAdapter(this, cursor);
-        tripView.setAdapter(tripCursorAdapter);
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mTripCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mTripCursorAdapter.swapCursor(null);
     }
 }
